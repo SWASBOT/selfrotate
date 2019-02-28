6d1d35e03febf578b1ea885b42bc030f2f443c5d
Waynx <48100793+Waynx@users.noreply.github.com>
2019-02-28 21:52:02 +0100

    Update streaming.js

--- commands/streaming.js
+++ commands/streaming.js
@@ -4,6 +4,6 @@
 module.exports = (self) => {
   self.registerCommand('streaming', function (msg, args) {
     this.self.editStatus(this.config.defaultStatus.toLowerCase(), {name: args ? args.join(' ') : 'nothing', type: 1, url: 'https://www.twitch.tv/twitch'})
-    this.send(msg, '👌')
+    this.send(msg, 'Effectué !')
   })
 }
