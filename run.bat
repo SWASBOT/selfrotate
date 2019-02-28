650433518a82a58eee2c459b7bc3105571637956
Zebulon <42072211+ZebulonDesigns@users.noreply.github.com>
2019-02-28 18:01:52 +0100

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,7 @@
+@echo off
+:: Created by Zebulon (https://github.com/ZebulonDesigns) [ 28 / 02 / 2019 ] 
+:: -*Read LICENSE to know more about permissions*- 
+title HectoniaBot 
+:START 
+node self.js /MIN /B
+goto START 
